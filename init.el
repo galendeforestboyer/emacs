7db28f13abdbbdf7af3a3ba5e7d5beb65968e99b
Galen Boyer <boyer@broadinstitute.org>
2023-02-10 21:39:06 -0500

    ...

--- init.el
+++ init.el
@@ -5,6 +5,7 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(canlock-password "837287b2292e7f137f21aa6b01622c0444dbfd47")
+ '(ediff-window-setup-function 'ediff-setup-windows-plain)
  '(package-selected-packages '(terraform-mode)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
